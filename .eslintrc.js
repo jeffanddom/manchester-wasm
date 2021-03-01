7996b7cf0562e4bda84f6f9246f7babee64c5f8f
Jeff Lee <jeffomatic@gmail.com>
2021-02-28 19:59:18 -0800

    fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,9 @@ module.exports = {
     ecmaFeatures: { jsx: true },
     project: './tsconfig.eslint.json',
   },
-  settings: {
-    react: { version: 'detect' },
-  },
   extends: [
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
+    'prettier',
     'plugin:prettier/recommended',
   ],
   plugins: ['import'],
@@ -48,4 +44,3 @@ module.exports = {
     'sort-imports': ['error', { ignoreDeclarationSort: true }],
   },
 }
-
